5107b584a7b912026d1303509f4adb1076e846b0
chinmay <chinmay2997@gmail.com>
2019-08-10 13:43:51 +0530

    All User permissions Entered, and GPS message of high accuracy entered

--- app/src/main/java/com/dexterlabs/myapplication/MainActivity.java
+++ app/src/main/java/com/dexterlabs/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -20,7 +21,9 @@ import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.karan.churi.PermissionManager.PermissionManager;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -29,19 +32,27 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity implements LocationListener {
 
     private LocationManager locationManager;
-    //Button submitData;
     DatabaseReference databaseActivity;
-    EditText ED_Name,ED_School;
+    EditText ED_Name, ED_School;
     List<Address> SchoolLocation = null;
     String School_Location = null;
     String loc_Latitude = null, loc_Longitude = null;
 
-    String name; String school; String timestamp; String day;
+    String name;
+    String school;
+    String timestamp;
+    String day;
+
+    PermissionManager permissionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        permissionManager = new PermissionManager(){};
+        permissionManager.checkAndRequestPermissions(this);
+
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
@@ -78,15 +89,22 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         getLocation();
 
 
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        ArrayList<String> granted = permissionManager.getStatus().get(0).granted;
+        ArrayList<String>denied = permissionManager.getStatus().get(0).denied;
     }
 
     void getLocation() {
         try {
             locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 5, this);
-        }
-        catch(SecurityException e) {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1, this);
+            Toast.makeText(this, "Location Gained",Toast.LENGTH_SHORT).show();
+        } catch (SecurityException e) {
             e.printStackTrace();
         }
 
@@ -109,10 +127,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
 
 
-            Date currentTime = Calendar.getInstance().getTime();
-
-            timestamp = currentTime.toString();
-            day = timestamp.substring(0,3);
+            Log.e("School Location",addresses.toString());
 
             SchoolLocation = addresses;
 
@@ -124,18 +139,17 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             addresses = null;
 
 
-         //    Toast.makeText(this,School_Location + loc_Longitude + loc_Latitude + day,Toast.LENGTH_LONG).show();
+            //    Toast.makeText(this,School_Location + loc_Longitude + loc_Latitude + day,Toast.LENGTH_LONG).show();
             // Toast.makeText(this,String.valueOf(currentTime) + String.valueOf(addresses),Toast.LENGTH_LONG).show();
-           // Toast.makeText(this,String.valueOf(location.getLatitude()),Toast.LENGTH_SHORT).show();
+            // Toast.makeText(this,String.valueOf(location.getLatitude()),Toast.LENGTH_SHORT).show();
 
-           // Toast.makeText(this,String.valueOf(addresses),Toast.LENGTH_LONG).show();
+            // Toast.makeText(this,String.valueOf(addresses),Toast.LENGTH_LONG).show();
 
 
-         //   locationText.setText(locationText.getText() + "\n"+addresses.get(0).getAddressLine(0)+", "+
+            //   locationText.setText(locationText.getText() + "\n"+addresses.get(0).getAddressLine(0)+", "+
 
-         //   addresses.get(0).getAddressLine(1)+", "+addresses.get(0).getAddressLine(2));
-        }catch(Exception e)
-        {
+            //   addresses.get(0).getAddressLine(1)+", "+addresses.get(0).getAddressLine(2));
+        } catch (Exception e) {
 
         }
 
@@ -161,16 +175,15 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     public void punchAttendance(View view) {
 
 
-        ED_Name = (EditText)findViewById(R.id.CoachName);
-        ED_School = (EditText)findViewById(R.id.CoachSchool);
+        ED_Name = (EditText) findViewById(R.id.CoachName);
+        ED_School = (EditText) findViewById(R.id.CoachSchool);
 
         name = ED_Name.getText().toString();
         school = ED_School.getText().toString();
 
-        if(name.equals("") || school.equals("") ){
-            Toast.makeText(this,"Please enter valid details",Toast.LENGTH_SHORT).show();
-        }
-        else{
+        if (name.equals("") || school.equals("")) {
+            Toast.makeText(this, "Please enter valid details", Toast.LENGTH_SHORT).show();
+        } else {
 
 //            if (School_Location.equals("")||day.equals("")){
 //                Toast.makeText(this,"Please Enable GPS and Internet or give permission of GPS to the app",Toast.LENGTH_LONG).show();
@@ -178,6 +191,18 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 //            }
 //
 //            else {
+
+
+           if (School_Location==null) {
+                Toast.makeText(this, "Please Restart the Application with Active Internet and GPS ", Toast.LENGTH_SHORT).show();
+
+            } else {
+
+                Date currentTime = Calendar.getInstance().getTime();
+
+                timestamp = currentTime.toString();
+                day = timestamp.substring(0, 3);
+
                 String id = databaseActivity.push().getKey();
 
 
@@ -185,10 +210,12 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
                 databaseActivity.child(id).setValue(model);
 
-                Toast.makeText(this,"Attendance Punched Successfully!",Toast.LENGTH_SHORT).show();
-//            }
+                Toast.makeText(this, "Attendance Punched Successfully!", Toast.LENGTH_SHORT).show();
+
+            }
+
+
         }
+    }   // end of Punch in
 
-
-    }
 }
